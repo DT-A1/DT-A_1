e57ce7f6eed76f1e79e3aba0c71e271ac4dee9ed
trung16861 <trung16861@users.noreply.github.com>
2019-10-28 22:49:23 +0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,23 +1,35 @@
 ﻿#pragma once
 class Matrix
 {
+private:
 	double** mat;
 	int nrows;
 	int ncols;
 public:
-	void input();
+	void input(); //Nhập ma trận
+	void inputEquations(); //Nhập hệ phương trình tuyến tính ở dạng ma trận hóa
 	void print(); //In ra ma tran
 	void interchangeRows(int row1, int row2); //Doi thu tu 2 dong
 	void addRowstoRow(double k, int row1, int row2);  //row2  = row2 + k*row1
 	void multiplyRow(double k, int row); //row = k*row
-	Matrix findLadderMatrix(); //Tim ma tran bac thang
+	Matrix findEchelonForm(); //Tim ma tran bac thang
+	Matrix findReducedEchelonForm(); //Tìm ma trận bậc thang rút gọn
 	double findDeterminant(); //Tim dinh thuc cua ma tran
-	Matrix cutRownCol(int row, int col); //Tra ve ma tran da bo di dong row, cot col
+	Matrix submatrix(int row, int col); //Tra ve ma tran da bo di dong row, cot col
 	int findRank(); //Tìm hạng của ma trận
+	Matrix multiplyMatrix(Matrix other); //Nhân với ma trận khác
+	bool isRevertible(); //Kiểm tra tính khả nghịch
+	Matrix findReverse(); //Tìm ma trận khả nghịch
+	static void solveLinearEquation(Matrix a);
+	double* operator[](int i);
+private:
 	bool isZeroRow(int row); //Kiểm tra dòng row có phải dòng không hay không
+	int leadingEntryPos(int row); //Vị trí của phần tử khác 0 đầu tiên của dòng
 
+public:
 	Matrix();
 	Matrix(int nrows, int ncols, int value);
+	Matrix(int nrows, int ncols);
 	Matrix(const Matrix& matrix);
 	~Matrix();
 };
