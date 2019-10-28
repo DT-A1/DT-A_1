189bec099b2b05abb281635650481b7c8f01d018
trung16861 <trung16861@users.noreply.github.com>
2019-10-28 22:49:37 +0700

    Create Vector.h

---
+++ Vector.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Vector
+{
+	double* vec;
+	int length;
+public:
+	int Length();
+	void input();
+	void print();
+	Vector operator+(const Vector& other);
+	Vector& operator+=(const Vector& other);
+	Vector operator*(const double& alpha);
+	Vector& operator*=(const double& alpha);
+	Vector();
+	Vector(int n);
+	~Vector();
+};
+
