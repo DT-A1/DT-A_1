0fab20c1d4eb8a9691c06eece551bc7f4e3ece3f
trung16861 <trung16861@users.noreply.github.com>
2019-10-28 22:49:27 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include "Matrix.h"
+#include "Vector.h"
 #include <iostream>
 using namespace std;
 
@@ -7,9 +8,36 @@ void main()
 	Matrix a;
 	a.input();
 	a.print();
-	//cout << "Deterninant: " << a.findDeterminant() << endl;
-	Matrix b = a.findLadderMatrix();
-	b.print();
+
+	//cout << "Echelon form: " << endl;
+	//Matrix b = a.findReducedEchelonForm();
+	//b.print();
 	//cout << "Matrix's rank: " << a.findRank() << endl;
+	//cout << "Deterninant: " << a.findDeterminant() << endl;
+	//cout << "Reverse: " << endl;
+	//a.findReverse().print();
+
+	/*Matrix b;
+	b.input();
+	b.print();
+
+	Matrix c;
+	c.input();
+	c.print();
+	
+	cout << endl;
+	a.multiplyMatrix(b).print();
+	b.multiplyMatrix(a).print();
+	a.multiplyMatrix(c).print();
+	c.multiplyMatrix(a).print();
+	b.multiplyMatrix(c).print();
+	c.multiplyMatrix(b).print();*/
+
+	//Matrix::solveLinearEquation(a);
+	/*Vector v1;
+	v1.input();
+	v1.print();*/
+
 	system("pause");
+
 }
