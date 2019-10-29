d293488e316053a4d5c91355d8702a211c8ab44b
trung16861 <trung16861@users.noreply.github.com>
2019-10-29 10:45:48 +0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -8,19 +8,19 @@ private:
 public:
 	void input(); //Nhập ma trận
 	void inputEquations(); //Nhập hệ phương trình tuyến tính ở dạng ma trận hóa
-	void print(); //In ra ma tran
-	void interchangeRows(int row1, int row2); //Doi thu tu 2 dong
+	void print(); //In ra ma trận
+	void interchangeRows(int row1, int row2); //Đổi vị trí 2 dòng nhận vào
 	void addRowstoRow(double k, int row1, int row2);  //row2  = row2 + k*row1
 	void multiplyRow(double k, int row); //row = k*row
-	Matrix findEchelonForm(); //Tim ma tran bac thang
+	Matrix findEchelonForm(); //Tìm ma trận bậc thang
 	Matrix findReducedEchelonForm(); //Tìm ma trận bậc thang rút gọn
-	double findDeterminant(); //Tim dinh thuc cua ma tran
-	Matrix submatrix(int row, int col); //Tra ve ma tran da bo di dong row, cot col
+	double findDeterminant(); //Tìm định thức của ma trận
+	Matrix submatrix(int row, int col); //Trả về ma trận đã bỏ đi dòng và cột nhận vào
 	int findRank(); //Tìm hạng của ma trận
 	Matrix multiplyMatrix(Matrix other); //Nhân với ma trận khác
 	bool isRevertible(); //Kiểm tra tính khả nghịch
 	Matrix findReverse(); //Tìm ma trận khả nghịch
-	static void solveLinearEquation(Matrix a);
+	static void solveLinearEquation();
 	double* operator[](int i);
 private:
 	bool isZeroRow(int row); //Kiểm tra dòng row có phải dòng không hay không
