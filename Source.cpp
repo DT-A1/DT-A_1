7ed3be3c3e6023655a70690cf8a0ed89945bf6f4
trung16861 <trung16861@users.noreply.github.com>
2019-10-31 19:48:29 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,43 +1,48 @@
 #include "Matrix.h"
 #include "Vector.h"
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 void main()
 {
-	Matrix a;
-	a.input();
-	a.print();
-
+	//Matrix a;
+	//a.inputEquations();
+	//a.input();
+	//a.print();
 	//cout << "Echelon form: " << endl;
-	//Matrix b = a.findReducedEchelonForm();
+	///Matrix b = a.findReducedEchelonForm();
 	//b.print();
 	//cout << "Matrix's rank: " << a.findRank() << endl;
 	//cout << "Deterninant: " << a.findDeterminant() << endl;
 	//cout << "Reverse: " << endl;
 	//a.findReverse().print();
 
-	/*Matrix b;
-	b.input();
-	b.print();
+	//Matrix b;
+	//b.input();
+	//b.print();
 
-	Matrix c;
-	c.input();
-	c.print();
-	
-	cout << endl;
-	a.multiplyMatrix(b).print();
-	b.multiplyMatrix(a).print();
-	a.multiplyMatrix(c).print();
-	c.multiplyMatrix(a).print();
-	b.multiplyMatrix(c).print();
-	c.multiplyMatrix(b).print();*/
+	//Matrix c;
+	//c.input();
+	//c.print();
+	//
+	//cout << endl;
+	//a.multiplyMatrix(b).print();
+	//b.multiplyMatrix(a).print();
+	//a.multiplyMatrix(c).print();
+	//c.multiplyMatrix(a).print();
+	//b.multiplyMatrix(c).print();
+	//c.multiplyMatrix(b).print();
 
 	//Matrix::solveLinearEquation(a);
-	/*Vector v1;
+	Vector v1, v2;
 	v1.input();
-	v1.print();*/
+	v1.print();
+	v2.input();
+	v2.print();
+
+	Vector v3 = v1 + v2;
+	v3.print();
 
 	system("pause");
-
 }
