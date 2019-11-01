f7cb06487ca5a48d2ade50ea555fe5228e11fbdc
trung16861 <trung16861@users.noreply.github.com>
2019-11-01 17:14:57 +0700

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 using namespace std;
 class Vector
@@ -9,10 +9,9 @@ public:
 	int Length();
 	void input();
 	void print();
-	Vector addVector(const Vector& other);
-	Vector operator+(const Vector& other);
+	Vector operator+(const Vector& other); //Cộng hai vector
 	Vector& operator+=(const Vector& other);
-	Vector operator*(const double& alpha);
+	Vector operator*(const double& alpha); //Nhân vector với một số alpha
 	Vector& operator*=(const double& alpha);
 	Vector& operator=(const Vector& v);
 	Vector();
